8d3854e1b105f8dbc63f15493cb84662903774a3
Adam Dickmeiss <adam@indexdata.dk>
2022-06-07 17:29:29 +0200

    OAI-PMH and xml-header without leading space

--- server/src/main/java/org/folio/metastorage/server/OaiService.java
+++ server/src/main/java/org/folio/metastorage/server/OaiService.java
@@ -31,7 +31,7 @@ public final class OaiService {
   private OaiService() { }
 
   static final String OAI_HEADER = """
-  <?xml version="1.0" encoding="UTF-8"?>
+      <?xml version="1.0" encoding="UTF-8"?>
       <OAI-PMH xmlns="http://www.openarchives.org/OAI/2.0/"
                xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
                xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/
