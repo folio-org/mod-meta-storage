348217050da263eca504e37a616cc281fc377f2d
Adam Dickmeiss <adam@indexdata.dk>
2022-06-11 18:50:22 +0200

    Unused imports

--- util/src/main/java/org/folio/metastorage/util/OaiParser.java
+++ util/src/main/java/org/folio/metastorage/util/OaiParser.java
@@ -1,8 +1,6 @@
 package org.folio.metastorage.util;
 
 import java.io.InputStream;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import javax.xml.stream.XMLInputFactory;
