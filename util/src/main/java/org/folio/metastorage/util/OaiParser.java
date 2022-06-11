c63526fde4beab9bea8bfe04b9e19d2931d47a27
Adam Dickmeiss <adam@indexdata.dk>
2022-06-11 12:21:10 +0200

    parseResponse with Consumer

--- util/src/main/java/org/folio/metastorage/util/OaiParser.java
+++ util/src/main/java/org/folio/metastorage/util/OaiParser.java
@@ -3,6 +3,7 @@ package org.folio.metastorage.util;
 import java.io.InputStream;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.function.Consumer;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
@@ -90,11 +91,22 @@ public class OaiParser {
   }
 
   /**
-   * Parse OAI-PMH response from InputStream.
+   * Parse OAI-PMH response from InputStream and save list.
    * @param stream stream
    * @throws XMLStreamException stream exception
    */
   public void parseResponse(InputStream stream) throws XMLStreamException {
+    parseResponse(stream, records::add);
+  }
+
+  /**
+   * Parse OAI-PMH response from InputStream and invoke for each record.
+   * @param stream stream
+   * @param recordHandler record consumer
+   * @throws XMLStreamException stream exception
+   */
+  public void parseResponse(InputStream stream, Consumer<OaiRecord> recordHandler)
+      throws XMLStreamException {
     XMLStreamReader xmlStreamReader = factory.createXMLStreamReader(stream);
     level = 0;
     OaiRecord lastRecord = null;
@@ -104,7 +116,7 @@ public class OaiParser {
         String elem = xmlStreamReader.getLocalName();
         if (level == 3 && ("record".equals(elem) || "header".equals(elem))) {
           if (lastRecord != null) {
-            records.add(lastRecord);
+            recordHandler.accept(lastRecord);
           }
           lastRecord = new OaiRecord();
         }
@@ -122,7 +134,8 @@ public class OaiParser {
       }
     }
     if (lastRecord != null) {
-      records.add(lastRecord);
+      recordHandler.accept(lastRecord);
     }
   }
+
 }
